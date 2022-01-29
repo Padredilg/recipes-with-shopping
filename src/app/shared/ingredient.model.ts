395915dc74cbadf667304ce1760f681d177fa4d2
Luiz <padredilg@knights.ucf.edu>
2022-01-28 22:13:40 -0500

    created shared/ingredient.model.ts and populated shopping-list and edit with prototypes

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient{
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number){
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    //same as the lines above for typescript
+    constructor(public name: string, public amount: number){}
+}
