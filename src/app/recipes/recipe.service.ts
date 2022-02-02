23b366eba649ee6127390f02c43bf3922a942f33
Luiz <padredilg@knights.ucf.edu>
2022-02-02 17:17:26 -0500

    included ingredients in recipe model, included sample ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,35 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
-    
+
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://static01.nyt.com/images/2021/03/28/dining/mc-shakshuka/mc-shakshuka-articleLarge.jpg'),
-        new Recipe('Another Test Recipe', 'This is simply another test', 'https://static01.nyt.com/images/2021/03/28/dining/mc-shakshuka/mc-shakshuka-articleLarge.jpg'),
+        new Recipe(
+            'Big Mac', 
+            'Mcdonalds Bigmac sandwich for your fat needs', 
+            'https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Big-Mac-1:product-header-mobile?wid=768&hei=441&dpr=off', 
+            [
+                new Ingredient('hamburgers', 2),
+                new Ingredient('shreaded lettuce', 1),
+                new Ingredient('american cheese slice', 2),
+                new Ingredient('BigMac Sauce', 1),
+                new Ingredient('Diced Onion', 1),
+                new Ingredient('Sliced pickles', 4),
+                new Ingredient('Bread with gergelim', 2),
+
+            ]
+        ),
+        new Recipe(
+            'Cup of coke', 
+            'learn how to prepare a cup of coke', 
+            'https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Coca-Cola-Classic-Small-1:product-header-mobile?wid=768&hei=441&dpr=off',
+            [
+                new Ingredient('Coke Bottle', 1),
+                new Ingredient('Cup', 1)
+            ]
+        ),
     ];
 
     getRecipes(){
